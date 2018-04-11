8911eca42fa59a02df6624aef707646d19f1e13e
Meriadec LANTONNET <mlantonn@student.42.fr>
2018-04-11 14:21:19 +0200

    removed useless steps

--- srcs/events/redraw.c
+++ srcs/events/redraw.c
@@ -6,7 +6,7 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/21 13:13:18 by mlantonn          #+#    #+#             */
-/*   Updated: 2018/03/23 20:06:37 by mlantonn         ###   ########.fr       */
+/*   Updated: 2018/04/11 14:19:22 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 void	redraw(t_all *all)
 {
-	mlx_destroy_image(all->data.mlx.mlx, all->data.mlx.img);
-	if (img_init(&all->data.mlx))
-		exit_all(all, -1);
 	draw_image(all);
 	mlx_put_image_to_window(all->data.mlx.mlx, all->data.mlx.win,
 		all->data.mlx.img, 0, 0);
